a024aa10ab6e53498e2e0fed6ad111f6249ce3e3
omarelf <omarelfellah456@gmail.com>
2023-10-04 14:53:14 +0100

    second commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_o5j63gt",
+        "template_9g5ob2f",
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Omar EL FELLAH",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "omarelfellah456@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "ZQWLWLFqsr3RTMYD-"
       )
       .then(
         () => {
